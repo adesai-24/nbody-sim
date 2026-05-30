2667579fee5d4e9f246bcec57089f17e47cc81a6
Aadi Desai <113480125+adesai-24@users.noreply.github.com>
2026-05-30 16:59:10 -0500

    Add body.h and body.c with initial implementations of body_init and trail_init functions

---
+++ src/body.c
@@ -0,0 +1,13 @@
+#include "body.h"
+
+Planetoid body_init(Vec3 pos, Vec3 vel, long double mass, long double radius, char *name) {
+    Planetoid new_planet;
+    new_planet.pos = new_planet.pos, new_planet.vel = vel, new_planet.mass = mass, new_planet.radius = radius, new_planet.name = name;
+    return new_planet;
+}
+
+Trail trail_init(int capacity) {
+    Trail trail;
+    trail.capacity = capacity, trail.count = 0, trail.head = 0;
+    return trail;
+}
---
+++ src/body.h
@@ -0,0 +1,9 @@
+#ifndef BODY_h
+#define BODY_h
+
+#include "objects/obj_types.h"
+
+Planetoid body_init(Vec3 pos, Vec3 vel, long double mass, long double radius, char *name);
+Trail trail_init(int capacity);
+
+#endif
