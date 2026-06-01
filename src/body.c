bff22f9f6c680e139753c0e99fb5cf76ab7b465f
Aadi Desai <113480125+adesai-24@users.noreply.github.com>
2026-06-01 16:52:39 -0500

    updated body to accomodate for allocated mem

--- src/body.c
+++ src/body.c
@@ -2,12 +2,19 @@
 
 Planetoid body_init(Vec3 pos, Vec3 vel, long double mass, long double radius, char *name) {
     Planetoid new_planet;
-    new_planet.pos = new_planet.pos, new_planet.vel = vel, new_planet.mass = mass, new_planet.radius = radius, new_planet.name = name;
+    new_planet.pos = new_planet.pos;
+    new_planet.vel = vel;
+    new_planet.mass = mass;
+    new_planet.radius = radius;
+    new_planet.name = name;
     return new_planet;
 }
 
-Trail trail_init(int capacity) {
-    Trail trail;
-    trail.capacity = capacity, trail.count = 0, trail.head = 0;
-    return trail;
+Trail *trail_init(int capacity) {
+    Trail *trail = malloc(sizeof(Trail) * capacity);
+    trail->capacity = capacity;
+    trail->head = 0;
+    trail->count = 0;
+    return trail; 
 }
+
