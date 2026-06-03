a6af2ca5c9883f29eb5d218ef2f921eba04bddc7
Aadi Desai <113480125+adesai-24@users.noreply.github.com>
2026-06-03 18:40:47 -0500

    Refactor vector operations: update vec_add to accept a Vec3 instead of a pointer and add vec_add_res function; adjust vec_scale and vec_scale_res signatures

--- src/obj_types.c
+++ src/obj_types.c
@@ -1,13 +1,21 @@
 #include "obj_types.h"
 #include <math.h>
 
-void *vec_add(Vec3 *dest, Vec3 *src) {
-    dest->x += src->x;
-    dest->y += src->y;
-    dest->z += src->z;
+void *vec_add(Vec3 *dest, Vec3 src) {
+    dest->x += src.x;
+    dest->y += src.y;
+    dest->z += src.z;
 }
 
-void vec_scale(Vec3 *vec, double scaler) {
+Vec3 vec_add_res(Vec3 v1, Vec3 v2) {
+    Vec3 res;
+    res.x = v1.x + v2.x;
+    res.y = v1.y + v2.y;
+    res.z = v1.z + v2.z;
+    return res;
+}
+
+void *vec_scale(Vec3 *vec, double scaler) {
     if (scaler <= 0.0) {
         printf("Please provide a value greater than 0!\n");
         return;
@@ -16,6 +24,18 @@ void vec_scale(Vec3 *vec, double scaler) {
     vec->y *= scaler;
     vec->z *= scaler;
 }
+
+Vec3 vec_scale_res(Vec3 vec, double scaler) {
+    if (scaler <= 0.0) {
+        printf("Please provide a value greater than 0!\n");
+        return;
+    }
+    vec.x *= scaler;
+    vec.y *= scaler;
+    vec.z *= scaler;
+    return vec;
+}
+
 double vec_magnitude(Vec3 vec) {
     return sqrt(pow(vec.x, 2) + pow(vec.y, 2) + pow(vec.z, 2));
 }
--- src/obj_types.h
+++ src/obj_types.h
@@ -16,8 +16,10 @@ typedef struct {
     int capacity, head, count;
 } Trail;
 
-void *vec_add(Vec3 *vec1, Vec3 *vec2);
-void vec_scale(Vec3 *vec, double scaler);
+void *vec_add(Vec3 *vec1, Vec3 vec2);
+Vec3 vec_add_res(Vec3 v1, Vec3 v2);
+void *vec_scale(Vec3 *vec, double scaler);
+Vec3 vec_scale_res(Vec3 vec, double scaler);
 double vec_magnitude(Vec3 vec);
 
 #endif
