16c20c4948ea0e428217da5b70ffe74fc331e193
Aadi <adesa44@illinois.edu>
2026-06-06 16:01:28 -0500

    added implementation for Barnes-Hut simulation via adding support for OctNode.

--- src/objects/obj_types.h
+++ src/objects/obj_types.h
@@ -1,6 +1,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdbool.h>
+
 typedef struct { 
     double x, y, z;
 } Vec3;
@@ -26,10 +28,33 @@ typedef struct {
     int    auto_fit;   // 1 = camera auto-rescales to keep all bodies in frame
 } SimCamera;
 
+typedef struct OctNode {
+    Vec3 center, half; // center point of the box, and half-width of the box
+    Vec3 com; // center of mass of everything in the tree
+    long double mass; // total mass of the tree 
+    int body_idx; // -1 means non-existent
+    struct OctNode *children[8];
+} OctNode;
+
+// extern variable definitions
+extern Planetoid *bodies;
+extern Trail *trails;
+extern OctNode *pool;
+extern int pool_idx;
+
+
+// Vec helper functions
 void vec_add(Vec3 *vec1, Vec3 vec2);
 Vec3 vec_add_res(Vec3 v1, Vec3 v2);
 void vec_scale(Vec3 *vec, double scaler);
 Vec3 vec_scale_res(Vec3 vec, double scaler);
 double vec_magnitude(Vec3 vec);
+
+// OctNode helper functions
+void tree_insert(OctNode *node, Planetoid *bodies, int body_idx);
+OctNode *create_child(OctNode *parent, int oct);
+int find_octant(Planetoid body, Vec3 node);
+OctNode *alloc_node();
+void free_node(OctNode *node);
 
 #endif
